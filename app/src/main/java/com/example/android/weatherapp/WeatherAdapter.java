52cd754370f0f1accf3735b10aa4efc079e88348
TanyaZagor <42569857+TanyaZagor@users.noreply.github.com>
2018-12-13 14:39:26 +0300

    RecyclerView

---
+++ app/src/main/java/com/example/android/weatherapp/WeatherAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.weatherapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
+
+    final Calendar calendar = Calendar.getInstance();
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView temperature;
+        private TextView date;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            temperature = itemView.findViewById(R.id.temperature);
+            date = itemView.findViewById(R.id.date);
+        }
+    }
+
+    @NonNull
+    @Override
+    public WeatherAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item, viewGroup, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherAdapter.ViewHolder viewHolder, int i) {
+
+        calendar.add(Calendar.DATE, +1);
+        viewHolder.temperature.setText("0 -10");
+        viewHolder.date.setText(DateFormat.getDateInstance().format(calendar.getTime()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+}
--- app/src/main/java/com/example/android/weatherapp/WeatherFragment.java
+++ app/src/main/java/com/example/android/weatherapp/WeatherFragment.java
@@ -1,17 +1,28 @@
 package com.example.android.weatherapp;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.util.Date;
 
 public class WeatherFragment extends Fragment {
+
     public static final String PARCEL = "parcel";
+    public String[] weather = {"Небольшой снег", "-1 -5", "700 мм", "3 м/с","80 %"};
+
+    private RecyclerView recyclerView;
+    private WeatherAdapter adapter;
 
     public static WeatherFragment create(Parcel parcel) {
         WeatherFragment f = new WeatherFragment();
@@ -25,6 +36,7 @@ public class WeatherFragment extends Fragment {
         Parcel parcel = (Parcel) getArguments().getSerializable(PARCEL);
         return parcel;
     }
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -45,11 +57,22 @@ public class WeatherFragment extends Fragment {
 
         cityNameView.setText(parcel.getCityName());
         dateView.setText(DateFormat.getDateInstance().format(date));
-        weatherView.setText("Небольшой снег");
-        temperatureView.setText("-1 -5");
-        pressureView.setText("700 мм");
-        windSpeedView.setText("3 м/с");
-        humidityView.setText("80 %");
+        weatherView.setText(weather[0]);
+        temperatureView.setText(weather[1]);
+        pressureView.setText(weather[2]);
+        windSpeedView.setText(weather[3]);
+        humidityView.setText(weather[4]);
+
+        recyclerView = layout.findViewById(R.id.recycler_view);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new WeatherAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
         return layout;
     }
 }
